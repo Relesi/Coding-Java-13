892f64614f9c9bb623400e922c91b856e1d9e926
relesi <renatolessa.2011@hotmail.com>
2024-07-01 11:28:16 -0300

    add json

--- src/main/java/com/coding/domain/Conta.java
+++ src/main/java/com/coding/domain/Conta.java
@@ -21,3 +21,25 @@ public class Conta {
     private List<Endereco> enderecos = new ArrayList<>();
 
 }
+
+// Aqui esta o Json usado em aula, caso queira testar...
+
+//    {
+//            "id": 1,
+//            "nome": "Renato",
+//            "nomeSocial": "Renato Lessa",
+//            "dataNascimento": "2023-10-09",
+//            "rg": 301286142,
+//            "cpf": 29403840988,
+//            "email": "renato.lessa@gmail.com",
+//            "enderecos": [
+//            {
+//            "nomeRua": "Rua Carlos",
+//            "numero": 212,
+//            "cep": "90909990",
+//            "cidade": "São Paulo",
+//            "estado": "São Paulo",
+//            "pais": "Brasil"
+//            }
+//            ]
+//            }
